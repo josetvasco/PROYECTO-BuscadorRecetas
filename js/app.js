8dd4661b06df8181650edbcbdee166aca0dd2d55
josetvasco <torovascojosedaniel@gmail.com>
2024-12-19 20:36:48 -0500

    Agregando las categorias traidas de la API al Select

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 function iniciarApp() {
 
-  obtenerCategoria()
+  const selectCategorias = document.querySelector('#categorias');
+
+  obtenerCategorias()
   
-  function obtenerCategoria() {
+  function obtenerCategorias() {
     const url = 'https://www.themealdb.com/api/json/v1/1/categories.php';
     fetch(url)
-      .then( respuesta => {
-        return respuesta.json()
-      })
-      .then( resultado => {
-        console.log(resultado)
-      })
+      .then( respuesta => respuesta.json())
+      .then( resultado => mostrarCategorias(resultado.categories))
+  }
+
+  function mostrarCategorias( categorias = [] ) {
+    categorias.forEach( categoria => {
+      const option = document.createElement('OPTION');
+      option.value = categoria.strCategory;
+      option.textContent = categoria.strCategory;
+      selectCategorias.appendChild(option);
+    })
   }
 
 }
