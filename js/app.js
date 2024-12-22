e4cf6649e474e0fb04497c845c45648ba5920199
josetvasco <torovascojosedaniel@gmail.com>
2024-12-22 15:11:24 -0500

    Mostrando la información de la receta escogida

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function iniciarApp() {
   selectCategorias.addEventListener('change', seleccionarCategoria);
 
   const resultado = document.querySelector('#resultado');
-  const modal = new bootstrap.Modal('#moda', {});
+  const modal = new bootstrap.Modal('#modal', {});
 
   obtenerCategorias()
   
@@ -87,8 +87,40 @@ function iniciarApp() {
       .then( resultado => mostrarRecetaModal(resultado.meals[0]) )
   }
 
-  function mostrarRecetaModal( rectea ) {
+  function mostrarRecetaModal( receta ) {
+    
+    const { idMeal, strInstructions, strMeal, strMealThumb } = receta;
 
+    const modalTitle = document.querySelector('.modal .modal-title');    
+    const modalBody = document.querySelector('.modal .modal-body');
+
+    modalTitle.textContent = strMeal;
+    modalBody.innerHTML = `
+      <img class="img-fluid" src="${strMealThumb}" alt="Receta ${strMeal}" />
+      <h3 class="my-3">Instrucciones</h3>
+      <p>${strInstructions}</p>
+      <h3 class="my-3">Ingredientes y cantidades</h3>
+    `
+
+    const listGroup = document.createElement('UL');
+    listGroup.classList.add('list-group');
+
+    for( let i = 1; i <= 20; i++ ) {
+      if(receta[`strIngredient${i}`]) {
+        const ingrediente = receta[`strIngredient${i}`];
+        const cantidad = receta[`strMeasure${i}`];
+
+        const ingredienteLi = document.createElement('LI');
+        ingredienteLi.classList.add('list-group-item');
+        ingredienteLi.textContent = `${ingrediente} - ${cantidad}`;
+
+        listGroup.appendChild(ingredienteLi);
+      }
+    }
+
+    modalBody.appendChild(listGroup);
+    
+    modal.show();
   }
   
   function limpiarHTML( selector ) {
