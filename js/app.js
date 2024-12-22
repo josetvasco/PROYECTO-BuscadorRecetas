ebf956a45043e12c7656daa09d6f26e51ffd407f
josetvasco <torovascojosedaniel@gmail.com>
2024-12-21 20:03:15 -0500

    Agregando la consulta al HTML e eliminado la consulta anterior para que no aparezca la consulta anterior

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 function iniciarApp() {
 
   const selectCategorias = document.querySelector('#categorias');
-  selectCategorias.addEventListener('change', seleccionarCategoria)
+  selectCategorias.addEventListener('change', seleccionarCategoria);
+
+  const resultado = document.querySelector('#resultado');
 
   obtenerCategorias()
   
@@ -26,7 +28,56 @@ function iniciarApp() {
     const url =  `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
     fetch(url)
       .then( respuesta => respuesta.json())
-      .then( resultado => console.log(resultado))
+      .then( resultado => mostrarRecetas(resultado.meals))
+  }
+
+  function mostrarRecetas( recetas = [] ) {
+
+    limpiarHTML(resultado);
+
+    const heading = document.createElement('H2');
+    heading.classList.add('text-center', 'text-black', 'my-5');
+    heading.textContent = recetas.length ? 'Resultados' : 'No Hay Resultados';
+    resultado.appendChild(heading)
+
+    recetas.forEach( receta => {
+      const { idMeal, strMeal, strMealThumb } = receta;
+
+      const recetaContenedor = document.createElement('DIV');
+      recetaContenedor.classList.add('col-md-4');
+
+      const recetaCard = document.createElement('DIV');
+      recetaCard.classList.add('card', 'mb-4');
+
+      const recetaImagen = document.createElement('IMG');
+      recetaImagen.classList.add('card-img-top');
+      recetaImagen.alt = `Imagen de la receta ${strMeal}`
+      recetaImagen.src = strMealThumb;
+
+      const recetaCardBody = document.createElement('DIV');
+      recetaCardBody.classList.add('card-body');
+
+      const recetaHeading = document.createElement('H3');
+      recetaHeading.classList.add('card-title', 'mb-3');
+      recetaHeading.textContent = strMeal;
+
+      const recetaButton = document.createElement('BUTTON');
+      recetaButton.classList.add('btn', 'btn-danger', 'w-100');
+      recetaButton.textContent = 'Ver Receta';
+
+      recetaCardBody.append(recetaHeading, recetaButton);
+      recetaCard.append(recetaImagen, recetaCardBody);
+
+      recetaContenedor.append(recetaCard);
+
+      resultado.appendChild(recetaContenedor);
+    })
+  }
+
+  function limpiarHTML( selector ) {
+    while(selector.firstChild) {
+      selector.removeChild(selector.firstChild);
+    }
   }
 
 }
